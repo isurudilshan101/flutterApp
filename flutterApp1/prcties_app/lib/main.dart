3bdde98df0e7a2539b0a17de32410f3f0a3c2f18
isurudilshan101 <40594182+isurudilshan101@users.noreply.github.com>
2019-12-19 13:09:35 +0530

    comments 3

--- flutterApp1/prcties_app/lib/main.dart
+++ flutterApp1/prcties_app/lib/main.dart
@@ -5,14 +5,28 @@ void main() => runApp(MaterialApp(
     appBar:AppBar(
       title:Text('my first app'),
       centerTitle: true,
+      backgroundColor: Colors.blue[700],
     ),
 
     body: Center(
-      child: Text('helo isuru'),
+      child: Text(
+        'hello isuru!!!',
+        style: TextStyle(
+           fontSize: 20.0,
+           fontWeight: FontWeight.bold,
+           letterSpacing: 2.0,
+           color: Colors.grey[600],
+          
+        ),
+
+      ),
     ),
     floatingActionButton: FloatingActionButton(
+      onPressed: () {},
       child: Text('click me'),
+      backgroundColor: Colors.red[600],
     ),
   ),
 ));
-  
+
+
